187806c74355a84037b6b65dbcb9f155d06be36c
James Cooke <james.cooke@cazoo.co.uk>
2021-06-04 11:22:33 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/gameOfLife.ts
+++ src/main/gameOfLife.ts
@@ -5,7 +5,52 @@ export class GameOfLife {
   constructor(board: Board) {
     this.board = board;
   }
-  nextGen(): void {}
+  nextGen(): void {
+      // this.board.forEach((boardRow, rowIndex) => {
+      //     boardRow.forEach((cell, colIndex) => {
+      //         let numberOfAliveNeighbours = 0;
+      //         let topLeftNeighbour = rowIndex > 0 && colIndex > 0 ? this.board[rowIndex - 1][colIndex - 1] : false;
+      //         let topMidNeighbour = rowIndex > 0 ? this.board[rowIndex - 1][colIndex] : false;
+      //         let topRightNeighbour = rowIndex < this.board.length && colIndex < this.board.length ? this.board[rowIndex - 1][colIndex + 1] : false;
+      //         let leftNeighbour = colIndex > 0 ? boardRow[colIndex - 1] : false;
+      //         let rightNeighbour = colIndex < this.board.length ? boardRow[colIndex + 1] : false;
+      //         let bottomLeftNeighbour = rowIndex < this.board.length && colIndex > 0 ? this.board[rowIndex + 1][colIndex - 1] : false;
+      //         let bottomMidNeighbour = rowIndex < this.board.length ? this.board[rowIndex + 1][colIndex] : false;
+      //         let bottomRightNeighbour = rowIndex < this.board.length && colIndex < this.board.length ? this.board[rowIndex + 1][colIndex + 1] : false;
+      //
+      //         if (topLeftNeighbour) {
+      //             numberOfAliveNeighbours++;
+      //         }
+      //         if (topMidNeighbour) {
+      //             numberOfAliveNeighbours++;
+      //         }
+      //         if (topRightNeighbour) {
+      //             numberOfAliveNeighbours++;
+      //         }
+      //         if (leftNeighbour) {
+      //             numberOfAliveNeighbours++;
+      //         }
+      //         if (rightNeighbour) {
+      //             numberOfAliveNeighbours++;
+      //         }
+      //         if (bottomLeftNeighbour) {
+      //             numberOfAliveNeighbours++;
+      //         }
+      //         if (bottomMidNeighbour) {
+      //             numberOfAliveNeighbours++;
+      //         }
+      //         if (bottomRightNeighbour) {
+      //             numberOfAliveNeighbours++;
+      //         }
+      //
+      //
+      //         if (numberOfAliveNeighbours !== 2 && numberOfAliveNeighbours !== 3) {
+      //             this.board[rowIndex][colIndex] = false;
+      //         }
+      //     })
+      // });
+  }
+
   printBoard(): string {
     return "-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-";
   }
--- src/test/gameOfLife.test.ts
+++ src/test/gameOfLife.test.ts
@@ -16,18 +16,33 @@ describe("Game Of Life", () => {
     );
   });
 
-  it("any live cell with fewer than two live neighbours dies", () => {
+  it("should initialise an board with one true of 5x5", () => {
     const gameOfLife = new GameOfLife([
-      [true, true, false, false, false],
-      [true, true, false, false, false],
-      [false, false, true, false, false],
+      [true, false, false, false, false],
+      [false, false, false, false, false],
+      [false, false, false, false, false],
       [false, false, false, false, false],
       [false, false, false, false, false],
     ]);
 
     gameOfLife.nextGen();
     expect(gameOfLife.printBoard()).toBe(
-      "+,+,-,-,-\n+,+,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-"
+      "+,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-"
     );
   });
+
+  // it("Any live cell with two or three live neighbours should live", () => {
+  //   const gameOfLife = new GameOfLife([
+  //     [true, true, false, false, false],
+  //     [true, false, false, false, false],
+  //     [false, false, false, false, false],
+  //     [false, false, false, false, false],
+  //     [false, false, false, false, false],
+  //   ]);
+  //
+  //   gameOfLife.nextGen();
+  //   expect(gameOfLife.printBoard()).toBe(
+  //     "+,+,-,-,-\n+,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-"
+  //   );
+  // });
 });
