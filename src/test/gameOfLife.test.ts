3b26b2443881b44401390d46efa48f668a5cc479
Panagiotis Smyrnis <panagiotis.smyrnis@cazoo.co.uk>
2021-06-04 11:40:41 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/gameOfLife.test.ts
+++ src/test/gameOfLife.test.ts
@@ -31,18 +31,18 @@ describe("Game Of Life", () => {
     );
   });
 
-  // it("Any live cell with two or three live neighbours should live", () => {
-  //   const gameOfLife = new GameOfLife([
-  //     [true, true, false, false, false],
-  //     [true, false, false, false, false],
-  //     [false, false, false, false, false],
-  //     [false, false, false, false, false],
-  //     [false, false, false, false, false],
-  //   ]);
-  //
-  //   gameOfLife.nextGen();
-  //   expect(gameOfLife.printBoard()).toBe(
-  //     "+,+,-,-,-\n+,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-"
-  //   );
-  // });
+  it("Any live cell with two or three live neighbours should live", () => {
+    const gameOfLife = new GameOfLife([
+      [true, true, false, false, false],
+      [true, false, false, false, false],
+      [false, false, false, false, false],
+      [false, false, false, false, false],
+      [false, false, false, false, false],
+    ]);
+
+    gameOfLife.nextGen();
+    expect(gameOfLife.printBoard()).toBe(
+      "+,+,-,-,-\n+,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-"
+    );
+  });
 });
