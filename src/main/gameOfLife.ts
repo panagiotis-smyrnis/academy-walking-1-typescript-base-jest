c5c61a86bf97737b2e961ccca835bfd1c3e423c3
Panagiotis Smyrnis <panagiotis.smyrnis@cazoo.co.uk>
2021-06-04 10:27:40 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/gameOfLife.ts
+++ src/main/gameOfLife.ts
@@ -1,6 +1,12 @@
+type Board = boolean[][];
+
 export class GameOfLife {
+  private board: Board;
+  constructor(board: Board) {
+    this.board = board;
+  }
   nextGen(): void {}
   printBoard(): string {
-    return "#,#,#,#,#\n#,#,#,#,#\n#,#,#,#,#\n#,#,#,#,#\n#,#,#,#,#";
+    return "-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-";
   }
 }
--- src/test/gameOfLife.test.ts
+++ src/test/gameOfLife.test.ts
@@ -2,11 +2,32 @@ import { GameOfLife } from "../main/gameOfLife";
 
 describe("Game Of Life", () => {
   it("should initialise an extinct board of 5x5", () => {
-    const gameOfLife = new GameOfLife([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]);
+    const gameOfLife = new GameOfLife([
+      [false, false, false, false, false],
+      [false, false, false, false, false],
+      [false, false, false, false, false],
+      [false, false, false, false, false],
+      [false, false, false, false, false],
+    ]);
 
     gameOfLife.nextGen();
     expect(gameOfLife.printBoard()).toBe(
-      "#,#,#,#,#\n#,#,#,#,#\n#,#,#,#,#\n#,#,#,#,#\n#,#,#,#,#"
+      "-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-"
+    );
+  });
+
+  it("any live cell with fewer than two live neighbours dies", () => {
+    const gameOfLife = new GameOfLife([
+      [true, true, false, false, false],
+      [true, true, false, false, false],
+      [false, false, true, false, false],
+      [false, false, false, false, false],
+      [false, false, false, false, false],
+    ]);
+
+    gameOfLife.nextGen();
+    expect(gameOfLife.printBoard()).toBe(
+      "+,+,-,-,-\n+,+,-,-,-\n-,-,-,-,-\n-,-,-,-,-\n-,-,-,-,-"
     );
   });
 });
