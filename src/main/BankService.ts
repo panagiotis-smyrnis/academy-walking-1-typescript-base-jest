47be89b1db2e9ae638536d90ea419a7ba95c2941
James Cooke <james.cooke@cazoo.co.uk>
2021-08-13 11:00:25 +0100

    adding deposit method

--- src/main/BankService.ts
+++ src/main/BankService.ts
@@ -1,13 +1,31 @@
 import { AccountService } from "./AccountService";
 
-export interface Printer {
-  print(): string;
+
+export interface Transaction {
+  date: string,
+  amount: number,
 }
+
+export interface Printer {
+  print(transactions: Transaction[]): string;
+}
+
+export class StringPrinter implements Printer {
+  public printBuffer: string = "";
+  print(transactions: Transaction[]): string {
+    this.printBuffer = "Date || Amount || Balance";
+    return "Date || Amount || Balance";
+  }
+
+}
+
 
 export class BankService implements AccountService {
   printer: Printer;
-  constructor(printer: Printer) {
+  private date: Date;
+  constructor(printer: Printer, date: Date) {
     this.printer = printer;
+    this.date = date;
   }
 
   deposit(amount: number): void {
@@ -17,6 +35,6 @@ export class BankService implements AccountService {
     throw new Error("Method not implemented.");
   }
   printStatement(): void {
-    throw new Error("Method not implemented.");
+    this.printer.print([]);
   }
 }
--- src/test/bankActions.test.ts
+++ src/test/bankActions.test.ts
@@ -1,13 +1,30 @@
-import { BankService, Printer } from "../main/BankService";
+import {BankService, Printer, StringPrinter} from "../main/BankService";
 import { mock } from "jest-mock-extended";
+import DateTimeFormat = Intl.DateTimeFormat;
 
 describe("Bank Service test", () => {
-  it("should print a default empty statement", async () => {
-    const printer = mock<Printer>();
-    printer.print.mockReturnValue("Date||Amount||Balance");
 
-    const bankService = new BankService(printer);
+  it("should print just the headers", async () => {
+    const printer = new StringPrinter();
+    const spy = jest.spyOn(printer, 'print');
+    const dateTime = mock<Date>();
+
+    const bankService = new BankService(printer, dateTime);
     bankService.printStatement();
-    expect(printer.print()).toBe("Date||Amount||Balance");
+    expect(spy).toHaveBeenCalled();
+    expect(printer.printBuffer).toBe("Date || Amount || Balance");
+  });
+
+  it("should print one deposit", async () => {
+    const printer = new StringPrinter();
+    const spy = jest.spyOn(printer, 'print');
+    const dateTime = mock<Date>();
+    dateTime.toDateString.mockReturnValue('10/01/2012');
+
+    const bankService = new BankService(printer, dateTime);
+    bankService.deposit(1000);
+    bankService.printStatement();
+    expect(spy).toHaveBeenCalled();
+    expect(printer.printBuffer).toBe("Date || Amount || Balance\n10/01/2012 || 1000 || 1000");
   });
 });
